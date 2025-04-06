cdf24ab1a2d69ee9112756187bd3e48907341ad1
bang-jeeewon <giw0233@gmail.com>
2025-04-06 20:59:23 +0900

    Page Router > 11) SSR 1. 소개 및 실습

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,12 +1,41 @@
 // import './index.css' // App 컴포넌트가 아닌 다른 데서 global css를 import 할 수 없음
 // NOTE: CSS Module을 사용하면, css class에 유니크한 해시값을 붙여주기 때문에 global css를 import할 수 있음
-import { ReactNode } from 'react'
+import { ReactNode, useEffect } from 'react'
 import style from './index.module.css'
 import SearchableLayout from '@/components/searchable-layout'
 import books from '@/mock/books.json'
 import BookItem from '@/components/book-item'
+import { InferGetServerSidePropsType } from 'next'
 
-export default function Home() {
+/**
+ * 약속된 함수 이름인 getServerSideProps를 만들어서 export 해주면 SSR로 동작하게 됨
+ * Home 컴포넌트보다 먼저 실행되어서
+ * 필요한 데이터를 또 다른 서버로부터 불러온다던가 하는 기능을 하는 함수
+ * pre-rendering하는 과정에서 딱 한번만 실행이 될 것이기 때문에, 서버측에서 실행되는 함수라는걸 알고있어야 함
+ */
+export const getServerSideProps = () => {
+  // 페이지 역할하는 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터 불러오는 함수
+
+  console.log('getServerSideProps') // 브라우저 측에서 출력이 안됨. 터미널에서 출력됨
+
+  const data = 'hello'
+
+  return {
+    props: {
+      data,
+    },
+  }
+}
+
+// Next.js에서 제공하는 알아서 타입을 추론해주는 InferGetServerSidePropsType
+export default function Home({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  // console.log(data) // 서버에서 한번, 브라우저에서 한번 출력됨
+
+  // 브라우저에서만 실행하고 싶은 코드라면 useEffect로 마운트 시점에 실행하면 됨
+  useEffect(() => {
+    console.log(window)
+  }, [])
+
   return (
     <div className={style.container}>
       <section>
